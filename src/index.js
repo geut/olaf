654019f1c68d812fb2a0405b52201dd777a40bb3
Martín Acosta <tinchoz49@gmail.com>
2018-09-29 11:02:38 -0300

    Testing

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
-var choo = require('choo')
+var swarm = require('webrtc-swarm')
+var signalhub = require('signalhub')
 
-var app = choo()
-if (process.env.NODE_ENV !== 'production') {
-  app.use(require('choo-devtools')())
-} else {
-  app.use(require('choo-service-worker')())
+var hub = signalhub('test-webrtc', ['https://plaid-jacket.glitch.me/'])
+
+var sw = swarm(hub)
+
+sw.on('peer', function (peer, id) {
+  console.log('connected to a new peer:', id)
+  console.log('total peers:', sw.peers.length)
+})
+
+sw.on('disconnect', function (peer, id) {
+  console.log('disconnected from a peer:', id)
+  console.log('total peers:', sw.peers.length)
+})
+
+if (module.hot) {
+  module.hot.accept(function () {
+    window.location.reload()
+  })
 }
-
-app.use(require('./stores/chat'))
-
-app.route('/', require('./views/main'))
-app.route('/*', require('./views/404'))
-
-module.exports = app.mount('body')
